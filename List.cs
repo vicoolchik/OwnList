f66b7c8ef215f688eae543799d2d6bae09738269
vicoolchik <78905683+vicoolchik@users.noreply.github.com>
2022-06-26 23:29:18 +0300

    Add files via upload

---
+++ List.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnList
+{
+    class MyList<T>
+    {
+        private T[] data;
+        private int size = 0;
+        private int capacity;
+
+        public MyList(int initialCapacity = 8)
+        {
+            if (initialCapacity < 1) initialCapacity = 1;
+            this.capacity = initialCapacity;
+            data = new T[initialCapacity];
+        }
+
+        public int Size { get { return size; } }
+        public bool IsEmpty { get { return size == 0; } }
+
+        public T GetAt(int index)
+        {
+            ThrowIfIndexOutOfRange(index);
+            return data[index];
+        }
+
+        public void SetAt(T newElement, int index)
+        {
+            ThrowIfIndexOutOfRange(index);
+            data[index] = newElement;
+        }
+
+        public void InsertAt(T newElement, int index)
+        {
+            ThrowIfIndexOutOfRange(index);
+            if (size == capacity)
+            {
+                Resize();
+            }
+
+            for (int i = size; i > index; i--)
+            {
+                data[i] = data[i - 1];
+            }
+
+            data[index] = newElement;
+            size++;
+        }
+        public string ToString()
+        {
+            string temp="";
+            for (int i = 0; i < size; i++)
+            {
+                temp += $"{data[i]} ; ";
+            }
+            return temp;
+        }
+
+        public void DeleteAt(int index)
+        {
+            ThrowIfIndexOutOfRange(index);
+            for (int i = index; i < size - 1; i++)
+            {
+                data[i] = data[i + 1];
+            }
+
+            data[size - 1] = default(T);
+            size--;
+        }
+
+        public void Add(T newElement)
+        {
+            if (size == capacity)
+            {
+                Resize();
+            }
+
+            data[size] = newElement;
+            size++;
+        }
+
+        public bool Contains(T value)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                T currentValue = data[i];
+                if (currentValue.Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Clear()
+        {
+            data = new T[capacity];
+            size = 0;
+        }
+
+        private void Resize()
+        {
+            T[] resized = new T[capacity * 2];
+            for (int i = 0; i < capacity; i++)
+            {
+                resized[i] = data[i];
+            }
+            data = resized;
+            capacity = capacity * 2;
+        }
+
+        public void ListsCombined(MyList<T> data2)
+        {
+            //if(this.GetType()!=data2.GetType())
+            //{
+            //    throw new TypeAccessException(string.Format("Cannot merge two lists because of unsuitable type"));
+            //}
+            T[] combined = new T[capacity * 2];
+            while (this.capacity - this.size < data2.size)
+            {
+                this.Resize();
+            }
+            for (int i = 0; i < this.size; i++)
+            {
+                combined[i] = data[i];
+            }
+
+            for (int i = this.size, y=0; y < data2.size; y++,i++)
+            {
+                combined[i] = data2.GetAt(y);
+            }
+            data = combined;
+            this.size += data2.size;
+        }
+
+        private void ThrowIfIndexOutOfRange(int index)
+        {
+            if (index > size - 1 || index < 0)
+            {
+                throw new ArgumentOutOfRangeException(string.Format("The current size of the array is {0}", size));
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list1 = new MyList<int>();
+            MyList<int> list2 = new MyList<int>();
+
+            list1.Add(1);
+            list1.Add(2);
+            list1.Add(3);
+            list1.Add(4);
+            list1.Add(5);
+
+            Console.WriteLine(list1.ToString());
+            Console.WriteLine(list1.GetAt(4));
+            list1.SetAt(6, 4);
+            Console.WriteLine(list1.ToString());
+            list1.InsertAt(5, 4);
+            Console.WriteLine(list1.ToString());
+            list1.DeleteAt(5);
+            Console.WriteLine(list1.ToString());
+            Console.WriteLine(list1.Contains(6));
+            Console.WriteLine(list1.Contains(5));
+
+            list2.Add(6);
+            list2.Add(7);
+            list2.Add(8);
+            list2.Add(9);
+            list2.Add(10);
+
+            list1.ListsCombined(list2);
+            Console.WriteLine(list1.ToString());
+            Console.WriteLine(list1.Size);
+            list1.Clear();
+            Console.WriteLine(list1.IsEmpty);
+            Console.WriteLine(list1.Size);
+
+            ///list1.SetAt(100, 100);
+        }
+    }
+}
